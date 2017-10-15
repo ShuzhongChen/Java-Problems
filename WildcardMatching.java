167beb4c260bab73e97a7f4e8a711c86930f0dc5
“ShuzhongChen” <chenshuzhongs@gmail.com>
2017-10-15 11:48:15 -0700

    WildcardMatching

---
+++ WildcardMatching.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+
+        for (int i = 1; i <= p.length(); i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    for (int k = i; k >= 0; k--) {
+                        if (dp[k][j - 1]) {
+                            dp[i][j] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[s.length()][p.length()];
+    }
+}
