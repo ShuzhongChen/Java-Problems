2bc34a968848cdae9ccfe58002c954f771e21a27
“ShuzhongChen” <chenshuzhongs@gmail.com>
2017-11-10 20:44:35 -0800

    Implement Stack using Queues

---
+++ ReverseLinkedList.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = tmp;
+        }
+
+        return newHead;
+    }
+}
